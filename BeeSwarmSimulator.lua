99fe0eeb75597a063588b97dce2283a922571895
ThortonEllmers <ellmersthorton@gmail.com>
2022-05-21 21:45:22 +1200

    Update BeeSwarmSimulator.lua

--- BeeSwarmSimulator.lua
+++ BeeSwarmSimulator.lua
@@ -1,2 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Bee Swarm Simulator", "GrapeTheme")
+
+local Tab = Window:NewTab("Main")
+local Tab = Window:NewTab("Farming")
+local Tab = Window:NewTab("Puffshrooms")
+local Tab = Window:NewTab("Planters")
+local Tab = Window:NewTab("Settings")
