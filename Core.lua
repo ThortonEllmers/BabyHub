ffa229c3c8ca48fe7173f1a96347fee5c0bb098d
ThortonEllmers <ellmersthorton@gmail.com>
2022-05-21 21:37:13 +1200

    Creating BabyHub
    Creating BabyHub

--- Core.lua
+++ Core.lua
@@ -1,4 +1,3 @@
 if game.PlaceId == 1537690962 then
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Bee Swarm Simulator", "GrapeTheme")
+    
 end
