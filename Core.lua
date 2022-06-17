3e44c8d8abb506d9d334e418d496a05ad4518304
ThortonEllmers <ellmersthorton@gmail.com>
2022-06-17 19:26:00 +1200

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -1,4 +1,8 @@
 -- Bee Swarm Script
 if game.PlaceId == 1537690962 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ThortonEllmers/BabyHub/main/BeeSwarmSimulator.lua"))()
+else if game.PlaceId == 7560156054 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ThortonEllmers/BabyHub/main/BeeSwarmSimulator.lua?token=GHSAT0AAAAAABUZRXYQFKV5HDRRT6NRUMNIYVJ6ICA"))()
+else
+	game.Players.LocalPlayer:Kick("\n\n⚠️   Error loading BabyHub   ⚠️\n\nReason:\nThe choosen game is not yet supported by BabyHub.\n\nNeed support?\nDiscord: coming soon\n")
 end
