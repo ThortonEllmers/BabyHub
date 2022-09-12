525f519d5f1fc1b62f633049a336ba7fc239f461
ThortonEllmers <ellmersthorton@gmail.com>
2022-09-13 05:33:49 +1200

    Create MotelSimulatorRedirect.lua

---
+++ MotelSimulatorRedirect.lua
@@ -0,0 +1,5 @@
+if _G.Beta == true then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ThortonEllmers/BabyHub-Non-Protected/main/Motel%20Simulator/TestForBeta/MotelSimulatorBetaRewrite.lua"))()
+else 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ThortonEllmers/BabyHub-Non-Protected/main/Motel%20Simulator/TestForBeta/MoteSimulatorNormalRewrite.lua"))()
+end
